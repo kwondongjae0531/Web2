2846a81b5022e61eed491fc8d7cfbeb9efcaff17
kwondongjae0531 <charlotte0531@naver.com>
2026-08-20 11:09:11 +0900

    Web2 initial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+import TodoListApp from "./01/TodoListApp";
+import reportWebVitals from './reportWebVitals'; //지우면안됨
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoListApp />
   </React.StrictMode>
 );
 
